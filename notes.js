529de3e495ef12a54668b5226875d3bf473f9702
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-24 11:48:34 +0200

    rendered weather icon

--- notes.js
+++ notes.js
@@ -1,6 +1,21 @@
 /* 
+-------------------API Calls--------------------------
+https://openweathermap.org/img/wn/${icon}@4x.png 
+<- to request weather icon of weather property in e.g. currentWeather object.
+
+------------------------------------------------------
+
+
 I can add more API calls later to expand on the App, like get weather by city name, etc. 
 Or make API calls to multiple cities and compare them. 
 
 rnf => Code snippet to create React Native functional component. 
+
+If confusion at line 7 of WeatherInfo component, remember this:   
+  const testing = [{ 1: 'one', 2: 'two', 3: 'three' }];
+  const [desctructered] = testing;
+
+console.log('destructered: ', desctructered); // Output: { 1: 'one', 2: 'two', 3: 'three' }
+
+
 */
